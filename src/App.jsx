7481e6df4747da7ea8708de77294aef5dc4bb0ed
KrishnaBalaji81 <krishnabalaji2207@gmail.com>
2025-02-03 13:12:04 +0530

    ADDED 2 FILES

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Revenue1 from './components/revenue1';
+import AddProject from './components/addproject';
+
+
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/revenue1" element={<Revenue1 />} />
+<Route path="/revenue1/addProject" element={<AddProject />} />
+
+
         </Routes>
       </div>
     </Router>
